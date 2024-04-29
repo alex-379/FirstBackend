a34299d4b24c7dc76a7676b882df6bf7ddaec240
alex-379 <alexey_379@mail.ru>
2024-04-30 01:57:12 +1000

    mv SwagerGen

--- FirstBackend.API/Extensions/ConfigureServices.cs
+++ FirstBackend.API/Extensions/ConfigureServices.cs
@@ -9,35 +9,7 @@ public static class ConfigureServices
     {
         services.AddControllers();
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(opt =>
-        {
-            opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
-            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                In = ParameterLocation.Header,
-                Description = "Please enter token",
-                Name = "Authorization",
-                Type = SecuritySchemeType.Http,
-                BearerFormat = "JWT",
-                Scheme = "bearer"
-            });
-
-            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type=ReferenceType.SecurityScheme,
-                            Id="Bearer"
-                        }
-                    },
-                    new string[]{}
-                }
-            });
-        });
-
+        services.AddSwager();
         services.AddExeptionsHandler();
         services.AddScoped<EnviromentVariables>();
         services.AddDataBases(enviromentVariables);
